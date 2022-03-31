d348ce392361a01dd60769a8d7b2f259ea1ef079
soopus <brandonharry11@gmail.com>
2022-03-31 12:48:25 -0600

    rectang

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ FPS = 60
 tick = 0
 
 
-def getRect():
-    return pygame.Rect((50, 50, 50, 50))
+def drawRect(pos):
+    pygame.draw.rect(DISPLAYSURF, (0, 0, 0), (pos[0], pos[1], 50, 50))
 
 
 def colorChange():
@@ -20,6 +20,7 @@ def colorChange():
     if tick == FPS:
         tick = 0
         DISPLAYSURF.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        drawRect([random.randint(0, 750), random.randint(0, 750)])
     else:
         tick += 1
 
@@ -31,8 +32,6 @@ while True:
             pygame.quit()
             print('l8r sk8r')
             sys.exit()
-
-    pygame.draw.rect(DISPLAYSURF, (0, 0, 0), getRect())
 
     pygame.display.update()
     fpsClock.tick(FPS)
