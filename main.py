9ba149923bc1dbfeb3e98ba1e531699c41cbeb30
C0Sine <braysambldzr@gmail.com>
2022-04-05 14:00:06 -0600

    created drawLine

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ FPS = 60
 # maskimage = pygame.transform.scale(pygame.image.load('pixil-frame-0.png'), (800, 800))
 # mask = pygame.mask.from_surface(maskimage)
 
+
 class Map(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -23,15 +24,15 @@ class Map(pygame.sprite.Sprite):
 testMap = Map()
 
 
-class wallTest(pygame.sprite.Sprite):
+class WallTest(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(pygame.image.load('wallTest2.png'), (800, 800))
+        self.image = pygame.transform.scale(pygame.image.load('shapeTest.png'), (200, 200))
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.mask.get_rect()
 
 
-weirdWall = wallTest()
+weirdWall = WallTest()
 weirdWall.rect.x, weirdWall.rect.y = 100, 100
 
 
@@ -56,6 +57,8 @@ class Player(pygame.sprite.Sprite):
 
 
 player = Player()
+fakePlayer = Player()
+# Fake player is an invisible "Player" used to detect collisions
 player.rect.x, player.rect.y = 300, 300
 
 while True:
@@ -78,10 +81,26 @@ while True:
         player.updatePosition(0, -5)
 
     if pygame.sprite.collide_mask(player, testMap) or pygame.sprite.collide_mask(player, weirdWall):
-        player.rect.x, player.rect.y = player.oldX, player.oldY
+        # Takes the fake player and, using the oldX or oldY, checks to see if it can move in the opposite direction
+        # For example, if it takes oldY, it will check to see if it could move left or right from the original position
+        # Any instance of which it would collide, returns it to the original X or Y, independently, allowing for diagonal movement
+        fakePlayer.rect.x, fakePlayer.rect.y = player.rect.x - 5, player.oldY
+        if pygame.sprite.collide_mask(fakePlayer, testMap) or pygame.sprite.collide_mask(fakePlayer, weirdWall):
+            player.rect.x = player.oldX
 
+        fakePlayer.rect.x, fakePlayer.rect.y = player.rect.x + 5, player.oldY
+        if pygame.sprite.collide_mask(fakePlayer, testMap) or pygame.sprite.collide_mask(fakePlayer, weirdWall):
+            player.rect.x = player.oldX
 
-    surface.blit(testMap.image, (0, 0))
+        fakePlayer.rect.x, fakePlayer.rect.y = player.oldX, player.rect.y - 5
+        if pygame.sprite.collide_mask(fakePlayer, testMap) or pygame.sprite.collide_mask(fakePlayer, weirdWall):
+            player.rect.y = player.oldY
+
+        fakePlayer.rect.x, fakePlayer.rect.y = player.oldX, player.rect.y + 5
+        if pygame.sprite.collide_mask(fakePlayer, testMap) or pygame.sprite.collide_mask(fakePlayer, weirdWall):
+            player.rect.y = player.oldY
+
+    surface.blit(testMap.image, testMap.rect)
     surface.blit(player.image, player.rect)
     surface.blit(weirdWall.image, weirdWall.rect)
     pygame.display.update()
