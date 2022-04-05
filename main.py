b2b4c9f07cd43c8f77ee7168838e5a3133eccbda
C0Sine <braysambldzr@gmail.com>
2022-04-05 13:04:43 -0600

    created drawLine

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ testMap = Map()
 class wallTest(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(pygame.image.load('wallTest.png'), (800, 800))
+        self.image = pygame.transform.scale(pygame.image.load('wallTest2.png'), (800, 800))
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.mask.get_rect()
 
@@ -42,20 +42,26 @@ class Player(pygame.sprite.Sprite):
         # Create a player mask from a given sprite
         self.image = pygame.image.load('datBoi.png')
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.mask.get_rect()
+        self.oldX, self.oldY = 0, 0
+        self.rect = self.image.get_rect()
         # X and Y position variables for player movement
         self.posX, self.posY = 300, 300
 
     def updatePosition(self, xDif, yDif):
         # Method that runs every tick to update the position if velX/velY != 0
-        self.posX += xDif
-        self.posY += yDif
+        self.rect.x += xDif
+        self.rect.y += yDif
+
+    # def updateSelf(self):
+    #     self.rect = pygame.image.
 
 
 player = Player()
+player.rect.x, player.rect.y = 300, 300
 
 while True:
     surface.fill((255, 255, 255))
+    player.oldX, player.oldY = player.rect[0], player.rect[1]
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -72,15 +78,12 @@ while True:
     if keyboard.is_pressed('w') or keyboard.is_pressed('Up'):
         player.updatePosition(0, -5)
 
-    if pygame.sprite.collide_mask(player, testMap):
-        player.updatePosition(0, 0)
-        print(str(testMap.mask.get_rect()) + "MASK RECT")
-        print(str(testMap.image.get_rect()) + "IMAGE RECT")
-        print("PEEOPLE")
+    if pygame.sprite.collide_mask(player, weirdWall):
+        player.rect.x, player.rect.y = player.oldX, player.oldY
 
 
     surface.blit(testMap.image, (0, 0))
-    surface.blit(player.image, (player.posX, player.posY))
+    surface.blit(player.image, player.rect)
     surface.blit(weirdWall.image, weirdWall.rect)
     pygame.display.update()
     fpsClock.tick(FPS)
