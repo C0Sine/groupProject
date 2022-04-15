c5d4a401b4ec3f0dccd44756e79bd8811a8b600a
soopus <brandonharry11@gmail.com>
2022-04-15 12:45:02 -0600

    Made better collisions and working on rotation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import keyboard as keyboard
 import pygame
 import random
 import sys
+import math
 
 from pygame import QUIT
 
@@ -58,16 +59,21 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         # Create a player mask from a given sprite
         self.image = pygame.transform.scale(pygame.image.load('player.png'), (40, 40))
-        self.hitbox = pygame.transform.scale(pygame.image.load('hitbox.png'), (40, 40))
+        self.image.set_colorkey((255, 255, 255))
+        self.hitbox = pygame.transform.scale(pygame.image.load('hitbox.png'), (30, 30))
         self.mask = pygame.mask.from_surface(self.hitbox)
-        self.rect = self.image.get_rect()
+        self.rect = self.hitbox.get_rect()
         # X and Y position variables for player movement
-        self.posX, self.posY = 300, 300
+        self.rect.x, self.rect.y = 100, 100
+        self.imageX = self.rect.x - abs((self.rect.width - self.image.get_width()) / 2)     # Changes image location to center hitbox
+        self.imageY = self.rect.y - abs((self.rect.height - self.image.get_height()) / 2)   # Changes image location to center hitbox
 
     def updatePosition(self, xDif, yDif):
         # Method that runs every tick to update the position if velX/velY != 0
         self.rect.x += xDif
         self.rect.y += yDif
+        self.imageX = self.rect.x - abs((self.rect.width - self.image.get_width()) / 2)     # Changes image location to center hitbox
+        self.imageY = self.rect.y - abs((self.rect.height - self.image.get_height()) / 2)   # Changes image location to center hitbox
 
     def updateCollisionPosition(self, direction):
         offset = (self.rect.x - testMap.rect.x, self.rect.y - testMap.rect.y)
@@ -80,16 +86,24 @@ class Player(pygame.sprite.Sprite):
                 self.rect.y -= self.mask.overlap_mask(testMap.mask, offset).get_rect().height - self.rect.width + 1
             if direction.__contains__('Up'):
                 self.rect.y += self.mask.overlap_mask(testMap.mask, offset).get_rect().height - self.rect.width + 1
-    # def updateSelf(self):
-    #     self.rect = pygame.image.
+        self.imageX = self.rect.x - abs((self.rect.width - self.image.get_width()) / 2)     # Changes image location to center hitbox
+        self.imageY = self.rect.y - abs((self.rect.height - self.image.get_height()) / 2)   # Changes image location to center hitbox
+
+
+def blitRotate(surf, image, topleft, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_image.set_colorkey((255, 255, 255))
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
+    surf.blit(rotated_image, new_rect.topleft)
 
 
 player = Player()
 fakePlayer = Player()
 # Fake player is an invisible "Player" used to detect collisions
-player.rect.x, player.rect.y = 100, 100
-playerspeed = 3
+player_speed = 3
 frame = 0
+mouse_x, mouse_y = 0, 0
+player_angle = 0
 while True:
     frame += 1
     surface.fill((255, 255, 255))
@@ -99,55 +113,62 @@ while True:
             pygame.quit()
             print('l8r sk8r')
             sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            mousex, mousey = pygame.mouse.get_pos()
 
     if True:    # Movement
         if (keyboard.is_pressed('a') or keyboard.is_pressed('Left')) and (keyboard.is_pressed('w') or keyboard.is_pressed('Up')):   # Diagonal movement
-            player.updatePosition(0 - round(playerspeed * 0.707), 0)
+            player.updatePosition(0 - round(player_speed * 0.707), 0)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Left')
-            player.updatePosition(0, 0 - round(playerspeed * 0.707))
+            player.updatePosition(0, 0 - round(player_speed * 0.707))
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Up')
         elif (keyboard.is_pressed('a') or keyboard.is_pressed('Left')) and (keyboard.is_pressed('s') or keyboard.is_pressed('Down')):  # Diagonal movement
-            player.updatePosition(0 - round(playerspeed * 0.707), 0)
+            player.updatePosition(0 - round(player_speed * 0.707), 0)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Left')
-            player.updatePosition(0, playerspeed * 0.707)
+            player.updatePosition(0, player_speed * 0.707)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Down')
         elif (keyboard.is_pressed('d') or keyboard.is_pressed('Right')) and (keyboard.is_pressed('s') or keyboard.is_pressed('Down')):  # Diagonal movement
-            player.updatePosition(playerspeed * 0.707, 0)
+            player.updatePosition(player_speed * 0.707, 0)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Right')
-            player.updatePosition(0, playerspeed * 0.707)
+            player.updatePosition(0, player_speed * 0.707)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Down')
         elif (keyboard.is_pressed('d') or keyboard.is_pressed('Right')) and (keyboard.is_pressed('w') or keyboard.is_pressed('Up')):  # Diagonal movement
-            player.updatePosition(playerspeed * 0.707, 0)
+            player.updatePosition(player_speed * 0.707, 0)
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Right')
-            player.updatePosition(0, 0 - round(playerspeed * 0.707))
+            player.updatePosition(0, 0 - round(player_speed * 0.707))
             if pygame.sprite.collide_mask(player, testMap):
                 player.updateCollisionPosition('Up')
         else:
             if keyboard.is_pressed('a') or keyboard.is_pressed('Left'):  # Cardinal movement
-                player.updatePosition(-playerspeed, 0)
+                player.updatePosition(-player_speed, 0)
                 if pygame.sprite.collide_mask(player, testMap):
                     player.updateCollisionPosition('Left')
             if keyboard.is_pressed('d') or keyboard.is_pressed('Right'):    # Cardinal movement
-                player.updatePosition(playerspeed, 0)
+                player.updatePosition(player_speed, 0)
                 if pygame.sprite.collide_mask(player, testMap):
                     player.updateCollisionPosition('Right')
             if keyboard.is_pressed('s') or keyboard.is_pressed('Down'):  # Cardinal movement
-                player.updatePosition(0, playerspeed)
+                player.updatePosition(0, player_speed)
                 if pygame.sprite.collide_mask(player, testMap):
                     player.updateCollisionPosition('Down')
             if keyboard.is_pressed('w') or keyboard.is_pressed('Up'):   # Cardinal movement
-                player.updatePosition(0, -playerspeed)
+                player.updatePosition(0, -player_speed)
                 if pygame.sprite.collide_mask(player, testMap):
                     player.updateCollisionPosition('Up')
 
     surface.blit(testMap.image, (0, 0))
-    surface.blit(pygame.transform.rotate(player.image, frame), player.rect)
+    if mouse_x > player.rect.x + (player.rect.width / 2):
+        target_angle = 270 - math.degrees(math.atan((mouse_y - player.rect.y - (player.rect.height / 2)) / (mouse_x - player.rect.x - (player.rect.width / 2))))
+    elif mouse_x < player.rect.x + (player.rect.width / 2):
+        target_angle = 90 - math.degrees(math.atan((mouse_y - player.rect.y - (player.rect.height / 2)) / (mouse_x - player.rect.x - (player.rect.width / 2))))
+    player_angle = frame
+    blitRotate(surface, player.image, (player.imageX, player.imageY), player_angle)
     pygame.display.update()
     fpsClock.tick(FPS)
